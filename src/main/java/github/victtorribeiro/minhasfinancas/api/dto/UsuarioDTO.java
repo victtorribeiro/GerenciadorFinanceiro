a2aad39dd283c01d0333eb869c3ead4120e4f621
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-03-20 20:02:19 -0300

    Adicionando construtor no usuarioDTO

--- src/main/java/github/victtorribeiro/minhasfinancas/api/dto/UsuarioDTO.java
+++ src/main/java/github/victtorribeiro/minhasfinancas/api/dto/UsuarioDTO.java
@@ -1,10 +1,15 @@
 package github.victtorribeiro.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 
     private String email;
