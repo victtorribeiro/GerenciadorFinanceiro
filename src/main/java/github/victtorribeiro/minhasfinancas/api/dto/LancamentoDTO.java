72cc270dfd99540301b5d03673733a1fbe5f2e2e
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-04-25 23:37:51 -0300

    Adicionando NoArgsConstructor e AllArgsConstructor

--- src/main/java/github/victtorribeiro/minhasfinancas/api/dto/LancamentoDTO.java
+++ src/main/java/github/victtorribeiro/minhasfinancas/api/dto/LancamentoDTO.java
@@ -1,12 +1,16 @@
 package github.victtorribeiro.minhasfinancas.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LancamentoDTO {
 
     private Long id;
