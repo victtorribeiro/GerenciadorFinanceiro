556a949b4ab2da70b7a2f2bf8e0789f583f31d93
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-05-30 21:37:39 -0300

    Hábilitando o a consulta de saldo por efetivados

--- src/main/java/github/victtorribeiro/minhasfinancas/domain/repository/LancamentoRepository.java
+++ src/main/java/github/victtorribeiro/minhasfinancas/domain/repository/LancamentoRepository.java
@@ -17,7 +17,7 @@ public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
     @Query(value =
             " select sum(l.valor) from Lancamento l join l.usuario u "
           + " where u.id = :idUsuario and l.tipo = :tipo and l.status = :status group by u ")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
             @Param("idUsuario") Long idUsuario,
             @Param("tipo") TipoLancamento tipo,
             @Param("status") StatusLancamento status
--- src/main/java/github/victtorribeiro/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/github/victtorribeiro/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -98,8 +98,8 @@ public class LancamentoServiceImpl implements LancamentoService {
     @Override
     @Transactional(readOnly = true)
     public BigDecimal obterSaldoPorUsuario(Long id) {
-        BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.RECEITA, StatusLancamento.PENDENTE);
-        BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuario(id, TipoLancamento.DESPESA, StatusLancamento.PENDENTE);
+        BigDecimal receitas = repository.obterSaldoPorTipoLancamentoEUsuarioEStatus(id, TipoLancamento.RECEITA, StatusLancamento.EFETIVADO);
+        BigDecimal despesas = repository.obterSaldoPorTipoLancamentoEUsuarioEStatus(id, TipoLancamento.DESPESA, StatusLancamento.EFETIVADO);
 
         if(receitas == null){
             receitas = BigDecimal.ZERO;
