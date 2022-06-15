d78b1ee6a65a9f469d54e2676eb2bdce05c4cf29
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-06-14 23:13:21 -0300

    adicionando a biblioteca Jwt e criando interface do JwtService

---
+++ src/main/java/github/victtorribeiro/minhasfinancas/service/JwtService.java
@@ -0,0 +1,17 @@
+package github.victtorribeiro.minhasfinancas.service;
+
+import github.victtorribeiro.minhasfinancas.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+
+public interface JwtService {
+
+    String gertarToken(Usuario usuario);
+
+    Claims obterClaims(String token) throws ExpiredJwtException;
+
+    boolean isTokenValido(String token);
+
+    String obterLoginUsuario(String token);
+
+}
