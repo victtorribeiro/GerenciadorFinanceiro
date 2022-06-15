fb5c7752d2b89ebe3ff76ee8d7b495ebf311f6e7
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-06-14 22:38:08 -0300

    Finalizando autenticação pelo spring security

--- src/main/java/github/victtorribeiro/minhasfinancas/service/impl/SecurityUserDetailsService.java
+++ src/main/java/github/victtorribeiro/minhasfinancas/service/impl/SecurityUserDetailsService.java
@@ -1,5 +1,8 @@
 package github.victtorribeiro.minhasfinancas.service.impl;
 
+import github.victtorribeiro.minhasfinancas.domain.entity.Usuario;
+import github.victtorribeiro.minhasfinancas.domain.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,9 +11,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityUserDetailsService implements UserDetailsService {
 
+    private UsuarioRepository usuarioRepository;
+
+    public SecurityUserDetailsService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        
-        return null;
+        Usuario usuarioEncontrado = usuarioRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email não cadastrado."));
+
+        return User.builder()
+                    .username(usuarioEncontrado.getEmail())
+                    .password(usuarioEncontrado.getSenha())
+                    .roles("USER")
+                    .build();
+
+
     }
 }
