44e09b7050ec29fbaa4404038126eee722c2b851
Victor Matheus Silva Ribeiro <victor_ribeiro96@outlook.com>
2022-06-18 22:24:32 -0300

    Adicionando configuração de CORS

--- src/main/java/github/victtorribeiro/minhasfinancas/config/SecurityConfiguration.java
+++ src/main/java/github/victtorribeiro/minhasfinancas/config/SecurityConfiguration.java
@@ -4,7 +4,9 @@ import github.victtorribeiro.minhasfinancas.api.JwtTokenFilter;
 import github.victtorribeiro.minhasfinancas.service.JwtService;
 import github.victtorribeiro.minhasfinancas.service.impl.SecurityUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +16,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -57,5 +65,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter(){
+        List<String> all = Arrays.asList("*");
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedMethods(all);
+        //config.setAllowedOrigins(all);
+        config.setAllowedOriginPatterns(all);
+        config.setAllowedHeaders(all);
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        CorsFilter corFilter = new CorsFilter(source);
+        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<CorsFilter>(corFilter);
+
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return filter;
+    }
 
 }
